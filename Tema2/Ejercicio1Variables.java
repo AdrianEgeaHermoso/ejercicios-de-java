8d0b51972aed13d4698f51e131101971c2980d76
Adrian Egea <egeaadrian14@gmail.com>
2020-10-20 10:15:22 +0200

    Añadido Tema2 Ejercicios 1 y 2

---
+++ Tema2/Ejercicio1Variables.java
@@ -0,0 +1,23 @@
+/**
+ * 2. Variables
+ *
+ * 1. Escribe un programa en el que se declaren las variables enteras x
+ *    e y. Asignales los valores 144 y 999 respectivamente. A continuación,
+ *    muestra por pantalla el valor de cada variable, la suma, la resta,
+ *    la división y la multiplicación.
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Ejercicio1Variables {
+  public static void main(String[] args) {
+    int x = 444;
+    int y = 211;
+    
+    System.out.println("x = " + x);
+    System.out.println("y = " + y);
+    System.out.println("x + y = " + (x + y));
+    System.out.println("x - y = " + (x - y));
+    System.out.println("x / y = " + ((float)x / (float)y));
+    System.out.println("x * y = " + (x * y));
+  }
+}
