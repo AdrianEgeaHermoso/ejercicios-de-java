541952a992e0adef1a71fd5274bafea7168ba3a4
Adrian Egea <egeaadrian14@gmail.com>
2020-11-03 10:00:38 +0100

    Ejercicios 1 y 4 del Tema 5

---
+++ Tema5/Ejercicio01Multiplos.java
@@ -0,0 +1,17 @@
+/**
+ * 5. Bucles
+ * 
+ * 1. Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ * 
+ * @author Adrián Egea Hermoso
+ */
+
+public class Ejercicio01Multiplos {
+
+  public static void main(String[] args) {
+    
+    for(int multiplo=0; multiplo < 100; multiplo += 5)
+    System.out.println(multiplo);
+    
+  }
+}
---
+++ Tema5/Ejercicio04Multiplos.java
@@ -0,0 +1,18 @@
+/**
+ * 5. Bucles
+ * 
+ * Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás utilizando
+ * un bucle for .
+ * 
+ * @author Adrián Egea Hermoso
+ */
+
+public class Ejercicio04Multiplos {
+
+  public static void main(String[] args) {
+    
+    for(int multiplo=320; multiplo > 160; multiplo -= 20)
+    System.out.println(multiplo);
+    
+  }
+}
