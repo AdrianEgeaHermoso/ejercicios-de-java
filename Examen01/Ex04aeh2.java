ded63beb9f5ce0b8bebf8e36d5befa557bbae4c2
Adrian Egea <egeaadrian14@gmail.com>
2020-11-24 09:32:19 +0100

    Ejercicios 2 y 3 del examen01

---
+++ Examen01/Ex04aeh2.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenreal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Adrián Egea Hermoso
+ */
+public class Ex04aeh2 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduzca la cantidad de paquetes que desea: ");
+    int cantidadPaquetes = Integer.parseInt(s.nextLine());
+    System.out.print("Numero de colores de la impresión de la taza: ");
+    int numeroColores = Integer.parseInt(s.nextLine());
+    System.out.print("Llavero plástico(1) Llavero metálico (2) ");
+    int llavero = Integer.parseInt(s.nextLine());
+
+    double precioUnitario = 4.90;
+
+    if (cantidadPaquetes >= 40 && cantidadPaquetes <= 99) {
+      precioUnitario = 3.90;
+    } else if (cantidadPaquetes >= 100) {
+      precioUnitario = 3.20;
+    }
+    
+
+    if (numeroColores > 3) {
+      precioUnitario = precioUnitario * 1.20;
+    }
+
+    double totalTazas = cantidadPaquetes * precioUnitario;
+
+    double precioLlavero;
+
+    if (llavero == 1) {
+      if (cantidadPaquetes < 50) {
+        precioLlavero = 2;
+      } else {
+        precioLlavero = 1.50;
+      }
+    }else{
+      if (cantidadPaquetes < 50) {
+        precioLlavero = 3;
+      } else {
+        precioLlavero = 2.50;
+      }
+    }
+    
+    double totalLlaveros = precioLlavero * cantidadPaquetes;
+    double tazaLLavero = totalTazas + totalLlaveros;
+    double iva = tazaLLavero * 0.21;
+    double total = tazaLLavero + iva;
+    
+    System.out.println("PROMOCIONES COMERCIALES - PRESUPUESTO");
+    System.out.println(" ");
+    System.out.printf("Taza                  %4dunid.\n", cantidadPaquetes);
+    System.out.printf("Precio unitario        %6.2f €\n", precioUnitario);
+    System.out.printf("Total  tazas           %6.2f €\n", totalTazas);
+    System.out.printf("Llaveros               %6.2f €\n", precioLlavero);
+    System.out.printf("Total llaveros         %6.2f €\n", totalLlaveros);
+    System.out.printf("Taza mas llaveros      %6.2f €\n", tazaLLavero);
+    System.out.printf("IVA                    %6.2f €\n", iva);
+    System.out.printf("Total                  %6.2f €\n", total);
+
+  }
+
+}
